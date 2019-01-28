3bd92306a5f3d843bf030b9fb72f136b3c2f94ed
amoi9 <aileenzhang7+github@gmail.com>
2019-01-27 20:11:49 -0800

    model and vedio

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
 import csv
 import cv2
 import numpy as np
+from sklearn.utils import shuffle
+from keras.models import Sequential
+from keras.layers import Flatten, Lambda, Dense, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
 
 data_path = "mydata/"
 def prepare_image(source_path):
@@ -8,62 +14,77 @@ def prepare_image(source_path):
     file_name = source_path.split('/')[-1]
     return cv2.cvtColor(cv2.imread(path + file_name), cv2.COLOR_BGR2RGB)
 
-lines = []
-with open(data_path + '/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+def read_samples():
+    lines = []
+    with open(data_path + '/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+    return lines[1:] 
 
-lines = lines[1:]
-images = []
-measurements = []
-for line in lines:
-    steering_center = float(line[3])
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    # create adjusted steering measurements for the side camera images
-    correction = 0.2 # this is a parameter to tune
-    steering_left = steering_center + correction
-    steering_right = steering_center - correction
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                steering_center = float(batch_sample[3])
 
-    # read in images from center, left and right cameras
-    img_center = prepare_image(line[0])
-    img_left = prepare_image(line[1])
-    img_right = prepare_image(line[2])
+                # create adjusted steering measurements for the side camera images
+                correction = 0.2 # this is a parameter to tune
+                steering_left = steering_center + correction
+                steering_right = steering_center - correction
 
-    # add images and angles to data set
-    images.extend((img_center, img_left, img_right))
-    measurements.extend((steering_center, steering_left, steering_right))
+                # read in images from center, left and right cameras
+                img_center = prepare_image(batch_sample[0])
+                img_left = prepare_image(batch_sample[1])
+                img_right = prepare_image(batch_sample[2])
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement * -1.0)
+                # add images and angles to data set
+                images.extend((img_center, img_left, img_right))
+                angles.extend((steering_center, steering_left, steering_right))
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+                # augment data set
+                images.extend((cv2.flip(img_center, 1), cv2.flip(img_left, 1), cv2.flip(img_right, 1)))
+                angles.extend((steering_center * -1.0, steering_left * -1.0, steering_right * -1.0))
 
-from keras.models import Sequential
-from keras.layers import Flatten, Lambda, Dense, Cropping2D
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
 
-model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+def train():
+    samples = read_samples()
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
 
-model.save('model.h5')
-exit()
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit_generator(train_generator, samples_per_epoch=
+                len(train_samples), validation_data=validation_generator,
+                nb_val_samples=len(validation_samples), nb_epoch=3)
+
+    model.save('model.h5')
+    exit()
+    
+train()
