d69fb596ba06a0972ae3d0466dba7d918101134a
amoi9 <aileenzhang7+github@gmail.com>
2019-01-27 20:48:50 -0800

    no generators

--- model.py
+++ model.py
@@ -1,12 +1,10 @@
 import csv
 import cv2
 import numpy as np
-from sklearn.utils import shuffle
 from keras.models import Sequential
 from keras.layers import Flatten, Lambda, Dense, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-from sklearn.model_selection import train_test_split
 
 data_path = "mydata/"
 def prepare_image(source_path):
@@ -22,46 +20,37 @@ def read_samples():
             lines.append(line)
     return lines[1:] 
 
-def generator(samples, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
+def generate_data(samples):
+    images = []
+    angles = []
+    for sample in samples:
+        steering_center = float(sample[3])
 
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                steering_center = float(batch_sample[3])
+        # create adjusted steering measurements for the side camera images
+        correction = 0.2 # this is a parameter to tune
+        steering_left = steering_center + correction
+        steering_right = steering_center - correction
 
-                # create adjusted steering measurements for the side camera images
-                correction = 0.2 # this is a parameter to tune
-                steering_left = steering_center + correction
-                steering_right = steering_center - correction
+        # read in images from center, left and right cameras
+        img_center = prepare_image(sample[0])
+        img_left = prepare_image(sample[1])
+        img_right = prepare_image(sample[2])
 
-                # read in images from center, left and right cameras
-                img_center = prepare_image(batch_sample[0])
-                img_left = prepare_image(batch_sample[1])
-                img_right = prepare_image(batch_sample[2])
+        # add images and angles to data set
+        images.extend((img_center, img_left, img_right))
+        angles.extend((steering_center, steering_left, steering_right))
 
-                # add images and angles to data set
-                images.extend((img_center, img_left, img_right))
-                angles.extend((steering_center, steering_left, steering_right))
+        # augment data set
+        images.extend((cv2.flip(img_center, 1), cv2.flip(img_left, 1), cv2.flip(img_right, 1)))
+        angles.extend((steering_center * -1.0, steering_left * -1.0, steering_right * -1.0))
 
-                # augment data set
-                images.extend((cv2.flip(img_center, 1), cv2.flip(img_left, 1), cv2.flip(img_right, 1)))
-                angles.extend((steering_center * -1.0, steering_left * -1.0, steering_right * -1.0))
-
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield shuffle(X_train, y_train)
+    X_train = np.array(images)
+    y_train = np.array(angles)
+    return X_train, y_train
 
 def train():
     samples = read_samples()
-    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-    # compile and train the model using the generator function
-    train_generator = generator(train_samples, batch_size=32)
-    validation_generator = generator(validation_samples, batch_size=32)
+    X_train, y_train = generate_data(samples)
 
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
@@ -80,9 +69,7 @@ def train():
     model.add(Dense(10))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
-    model.fit_generator(train_generator, samples_per_epoch=
-                len(train_samples), validation_data=validation_generator,
-                nb_val_samples=len(validation_samples), nb_epoch=3)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
     model.save('model.h5')
     exit()
